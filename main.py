a2069e268cca54eb81a5e0f990e04c09cb437ee6
George Nizor <gnizorid@gmail.com>
2025-08-05 20:24:39 +1000

    Add player and shot mechanics; define constants for player attributes

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 from constants import *
-
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -8,15 +11,43 @@ def main():
     print("Screen width:",SCREEN_WIDTH) 
     print("Screen height:",SCREEN_HEIGHT)
 
+    fps_timer = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = (updateable,)
+
+    shots = pygame.sprite.Group()
+    Shot.containers = (shots, updateable, drawable)
+
+    asteroid_field = AsteroidField()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
-
-
+        dt = fps_timer.tick(60) / 1000
+        updateable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collide(player):
+                print("Game over!")
+                return
+            for shot in shots:
+                if shot.collide(asteroid):
+                    shot.kill()
+                    asteroid.kill()
+        
     
 
 
---
+++ shot.py
@@ -0,0 +1,16 @@
+import pygame
+from circleshape import CircleShape
+from constants import SHOT_RADIUS
+
+class Shot(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        self.velocity = pygame.Vector2(0, 0)
+        self.position = pygame.Vector2(x, y)
+        self.radius = SHOT_RADIUS
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
